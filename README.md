6c3b6d225d6f1783929021bccf13dfe53653d895
PepVal <pepval001@gmail.com>
2020-04-25 20:03:40 -0500

    upload proyect

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # speech-without-dialog
+
+## Repositories and webs to guide me
+ - [Original Repository](https://github.com/Truiton/CustomSpeechRecognizer) click for more info
+ - [Truiton](https://www.truiton.com/2014/06/android-speech-recognition-without-dialog-custom-activity/) click for more info
+ - [Programmin-Books](https://www.programming-books.io/essential/android/speech-to-text-without-dialog-c40c68caaeae471abc08d80ebd76ad3a) click for more info
+ - [StackOverflow](https://stackoverflow.com/questions/38678163/voice-recognition-without-google-dialog) click for more info
---
+++ app/src/main/java/com/pepval/speechapi/MainActivity.java
@@ -0,0 +1,277 @@
+package com.pepval.speechapi;
+
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements RecognitionListener {
+
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    private static final int REQUEST_RECORD_PERMISSION = 100;
+
+    private TextView returnedText, txt_state;
+    private ToggleButton toggleButton;
+    private ProgressBar progressBar;
+    private SpeechRecognizer speech = null;
+    private Intent recognizerIntent;
+    private String LOG_TAG = "VoiceRecognitionActivity";
+
+    private Button btn_micro;
+    private TextView txt_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //setup view
+        btn_micro = findViewById(R.id.btn_micro);
+        txt_text = findViewById(R.id.txt_text);
+
+        //setup view #2
+        returnedText = findViewById(R.id.textView1);
+        progressBar = findViewById(R.id.progressBar1);
+        toggleButton = findViewById(R.id.toggleButton1);
+        txt_state = findViewById(R.id.txt_state);
+
+        progressBar.setVisibility(View.INVISIBLE);
+        speech = SpeechRecognizer.createSpeechRecognizer(this);
+        Log.i(LOG_TAG, "isRecognitionAvailable: " + SpeechRecognizer.isRecognitionAvailable(this));
+        speech.setRecognitionListener(this);
+        recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE,
+                "en");
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
+
+
+        //listener without google's banner
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setIndeterminate(true);
+                    ActivityCompat.requestPermissions
+                            (MainActivity.this,
+                                    new String[]{Manifest.permission.RECORD_AUDIO},
+                                    REQUEST_RECORD_PERMISSION);
+                } else {
+                    progressBar.setIndeterminate(false);
+                    progressBar.setVisibility(View.INVISIBLE);
+                    speech.stopListening();
+                }
+            }
+        });
+
+
+        //listener with google's banner
+        btn_micro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //speechInput();
+            }
+        });
+
+    }
+
+    //function for request permission
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_RECORD_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    speech.startListening(recognizerIntent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied!", Toast
+                            .LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    private void speechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txt_text.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
+
+    //state stop Activity
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (speech != null) {
+            speech.destroy();
+            Log.i(LOG_TAG, "destroy");
+        }
+    }
+
+
+    //implements methods of RecognitionListener
+    @Override
+    public void onReadyForSpeech(Bundle bundle) {
+        Log.i(LOG_TAG, "onReadyForSpeech");
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+        Log.i(LOG_TAG, "onBeginningOfSpeech");
+        progressBar.setIndeterminate(false);
+        progressBar.setMax(10);
+    }
+
+    @Override
+    public void onRmsChanged(float rmsdB) {
+        Log.i(LOG_TAG, "onRmsChanged: " + rmsdB);
+        progressBar.setProgress((int) rmsdB);
+    }
+
+    @Override
+    public void onBufferReceived(byte[] bytes) {
+        Log.i(LOG_TAG, "onBufferReceived: " + bytes);
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+        Log.i(LOG_TAG, "onEndOfSpeech");
+        progressBar.setIndeterminate(true);
+        toggleButton.setChecked(false);
+    }
+
+    @Override
+    public void onError(int errorCode) {
+        String errorMessage = getErrorText(errorCode);
+        Log.d(LOG_TAG, "FAILED " + errorMessage);
+        returnedText.setText(errorMessage);
+        toggleButton.setChecked(false);
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        Log.e(LOG_TAG, "onResults");
+        ArrayList<String> matches = results
+                .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+        assert matches != null;
+        if (matches.get(0).equals("Can you hear me")) {
+            returnedText.setText(matches.get(0));
+            returnedText.setTextColor(ContextCompat.getColor(this, R.color.successful));
+
+            txt_state.setText("Match");
+            txt_state.setVisibility(View.VISIBLE);
+            txt_state.setTextColor(ContextCompat.getColor(this,R.color.successful));
+            Toast.makeText(this, "Congratulations :D", Toast.LENGTH_LONG).show();
+        } else {
+            returnedText.setText(matches.get(0));
+            returnedText.setTextColor(ContextCompat.getColor(this, R.color.error));
+
+            txt_state.setText("Unmatch");
+            txt_state.setVisibility(View.VISIBLE);
+            txt_state.setTextColor(ContextCompat.getColor(this,R.color.error));
+            Toast.makeText(this, "Sorry, try again :(", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onPartialResults(Bundle bundle) {
+        Log.i(LOG_TAG, "onPartialResults");
+    }
+
+    @Override
+    public void onEvent(int i, Bundle bundle) {
+        Log.i(LOG_TAG, "onEvent");
+    }
+
+
+    //getErrorText Message
+    public static String getErrorText(int errorCode) {
+        String message;
+        switch (errorCode) {
+            case SpeechRecognizer.ERROR_AUDIO:
+                message = "Audio recording error";
+                break;
+            case SpeechRecognizer.ERROR_CLIENT:
+                message = "Client side error";
+                break;
+            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                message = "Insufficient permissions";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK:
+                message = "Network error";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                message = "Network timeout";
+                break;
+            case SpeechRecognizer.ERROR_NO_MATCH:
+                message = "No match";
+                break;
+            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                message = "RecognitionService busy";
+                break;
+            case SpeechRecognizer.ERROR_SERVER:
+                message = "error from server";
+                break;
+            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                message = "No speech input";
+                break;
+            default:
+                message = "Didn't understand, please try again.";
+                break;
+        }
+        return message;
+    }
+}
